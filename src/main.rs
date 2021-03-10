743049ef22485cddf069925a96ff614c550a8cd3
importly <aryankuthakur@hotmail.com>
2021-03-10 12:33:25 -0500

    pushed

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ fn functioner(x: f64) -> f64 {
     x * x
 }
 
+fn drawer(){
+    cli::graph::graph(functioner);
+}
+
 fn main() {
     let app = app::App::default();
 
     let mut win = window::Window::new(100, 100, 740, 580, "Mathical GUI");
 
     let mut graph = button::Button::new(160, 210, 80, 40, "Graph");
-
-    graph.set_callback(move || cli::graph::graph(functioner));
+    
+    //graph.set_callback
+    // graph.set_callback(move || cli::graph::graph(functioner));
 
     win.end();
     win.show();
