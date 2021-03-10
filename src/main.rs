c2922e6edd63fa3d46269c252485f935269fb12b
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-09 20:33:47 -0800

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use fltk::*;
+use mathical::{self, cli};
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+
+    let mut win = window::Window::new(100, 100, 740, 580, "Mathical GUI");
+
+    let mut graph = button::Button::new(160, 210, 80, 40, "Graph");
+
+    graph.set_callback(move || cli::graph::graph());
+
+    win.end();
+    win.show();
+    app.run().unwrap();
 }
