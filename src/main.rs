baa079ea9a2a74c8f2011167fe9bf1b1b768e629
importly <aryankuthakur@hotmail.com>
2021-03-10 12:38:41 -0500

    quick push

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,12 @@ fn main() {
 
     let mut win = window::Window::new(100, 100, 740, 580, "Mathical GUI");
 
+    let mut framer = frame::Frame::new(0, 0, 400, 200, "");
+
     let mut graph = button::Button::new(160, 210, 80, 40, "Graph");
     
-    //graph.set_callback
-    // graph.set_callback(move || cli::graph::graph(functioner));
+    graph.set_callback(move || framer.set_label("Ho"));
+    graph.set_callback(move || cli::graph::graph(functioner));
 
     win.end();
     win.show();
